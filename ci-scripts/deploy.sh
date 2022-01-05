c4e7d4fc9772a3d489f98ec1a533e28bf8e1464a
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2022-01-05 14:02:32 +0100

    remove print outs

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -158,18 +158,11 @@ echo "Application URL: http://${IP_ADDRESS}:${PORT}"
 #####################
 
 OAUTHTOKEN=$(ibmcloud iam oauth-tokens | awk '{print $4;}')
-echo $OAUTHTOKEN
-
-echo APPID_MANAGEMENT_URL
-echo ${APPID_MANAGEMENT_URL}
+#echo $OAUTHTOKEN
 APPID_MANAGEMENT_URL_ALL_REDIRECTS=${APPID_MANAGEMENT_URL}/config/redirect_uris
-echo $APPID_MANAGEMENT_URL_ALL_REDIRECTS
+#echo $APPID_MANAGEMENT_URL_ALL_REDIRECTS
 CURRENT_REDIRECT_URIS=$(curl -v -H "Content-Type: application/json" -H "Authorization: Bearer $OAUTHTOKEN" $APPID_MANAGEMENT_URL_ALL_REDIRECTS)
-echo "niklas here"
-echo $CURRENT_REDIRECT_URIS
-
+#echo $CURRENT_REDIRECT_URIS
 FRONTEND_URL="http://${IP_ADDRESS}:${PORT}"
-
-echo "adam here"
 echo $CURRENT_REDIRECT_URIS | jq -r '.redirectUris |= ['\"$FRONTEND_URL\"'] + .' > ./new-redirects.json
 result=$(curl -v -d @./new-redirects.json -H "Content-Type: application/json" -X PUT -H "Authorization: Bearer $OAUTHTOKEN" $APPID_MANAGEMENT_URL_ALL_REDIRECTS)
