951622076e04e729ca60d2e9abaf1468ce547517
Adam de Leeuw <deleeuw@uk.ibm.com>
2022-01-25 19:59:14 +0000

    t

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -284,7 +284,7 @@ APPID_MANAGEMENT_URL_ALL_REDIRECTS=${APPID_MANAGEMENT_URL}/config/redirect_uris
 CURRENT_REDIRECT_URIS=$(curl -v -H "Content-Type: application/json" -H "Authorization: Bearer $OAUTHTOKEN" $APPID_MANAGEMENT_URL_ALL_REDIRECTS)
 #echo $CURRENT_REDIRECT_URIS
 FRONTEND_URL="http://${APPURL}"
-echo "Adding the following URL to AppID redirect URLs: https://${FRONTEND_URL}"
+echo "Adding the following URL to AppID redirect URLs: ${FRONTEND_URL}"
 echo $CURRENT_REDIRECT_URIS | jq -r '.redirectUris |= ['\"$FRONTEND_URL\"'] + .' > ./new-redirects.json
 result=$(curl -v -d @./new-redirects.json -H "Content-Type: application/json" -X PUT -H "Authorization: Bearer $OAUTHTOKEN" $APPID_MANAGEMENT_URL_ALL_REDIRECTS)
 
@@ -311,7 +311,7 @@ else
   CURRENT_REDIRECT_URIS=$(curl -v -H "Content-Type: application/json" -H "Authorization: Bearer $OAUTHTOKEN" $APPID_MANAGEMENT_URL_ALL_REDIRECTS)
   #echo $CURRENT_REDIRECT_URIS
   FRONTEND_URL="http://${IP_ADDRESS}:${PORT}"
-  echo "Adding the following URL to AppID redirect URLs: https://${FRONTEND_URL}"
+  echo "Adding the following URL to AppID redirect URLs: ${FRONTEND_URL}"
   echo $CURRENT_REDIRECT_URIS | jq -r '.redirectUris |= ['\"$FRONTEND_URL\"'] + .' > ./new-redirects.json
   result=$(curl -v -d @./new-redirects.json -H "Content-Type: application/json" -X PUT -H "Authorization: Bearer $OAUTHTOKEN" $APPID_MANAGEMENT_URL_ALL_REDIRECTS)
 
